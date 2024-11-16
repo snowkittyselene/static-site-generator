c9ba9b8372a719fdbea59b6fe7f98d51eb8ac758
Selene Moonsilver <bobdablob42+gamedev@gmail.com>
2024-11-16 17:36:54 -0500

    Add repr to HTMLNode child classes

--- src/leafnode.py
+++ src/leafnode.py
@@ -18,3 +18,6 @@ class LeafNode(HTMLNode):
             return f"<{self.tag} {self.props_to_html()}>{self.value}</{self.tag}>"
         else:
             return f"<{self.tag}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, props: {self.props})"
--- src/parentnode.py
+++ src/parentnode.py
@@ -20,6 +20,9 @@ class ParentNode(HTMLNode):
             html += child.to_html()
         return html + f"</{self.tag}>"
 
+    def __repr__(self):
+        return f"ParentNode({self.tag}, Children: {self.children}, Props: {self.props})"
+
 
 class NoChildrenError(Exception):
     pass
