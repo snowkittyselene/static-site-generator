62b2c9c9536e347ebf87a2b578dcd932309573d5
Selene Moonsilver <bobdablob42+gamedev@gmail.com>
2024-11-16 02:08:36 -0500

    Add text node

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
-print("hello world")
+from textnode import *
+
+
+def main():
+    test_node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+    print(test_node)
+
+
+if __name__ == "__main__":
+    main()
