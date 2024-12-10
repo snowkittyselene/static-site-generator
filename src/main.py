c695b8b11213c25f80b5681a986d25703a92c735
Selene Moonsilver <bobdablob42+gamedev@gmail.com>
2024-12-09 19:22:26 -0500

    Add copy static to public function

--- src/main.py
+++ src/main.py
@@ -1,10 +1,32 @@
-from textnode import TextNode, TextType
-from htmlnode import HTMLNode, LeafNode, ParentNode
+import os, shutil
+
+
+PATH_STATIC = "./static"
+PATH_PUBLIC = "./public"
 
 
 def main():
-    # To be implemented
-    pass
+    copy_files(PATH_STATIC, PATH_PUBLIC)
+
+
+def copy_files(source, destination):
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+    copy_recursive(source, destination)
+
+
+def copy_recursive(source, destination):
+    # Make destination if doesn't exist
+    if not os.path.exists(destination):
+        os.mkdir(destination)
+
+    for file in os.listdir(source):
+        from_path = os.path.join(source, file)
+        dest_path = os.path.join(destination, file)
+        if os.path.isfile(from_path):
+            shutil.copy(from_path, dest_path)
+        else:
+            copy_recursive(from_path, dest_path)
 
 
 if __name__ == "__main__":
