0cb2d11a688da3cd6548b1a743b67e96904cdd76
Selene Moonsilver <bobdablob42+gamedev@gmail.com>
2024-12-10 23:33:44 -0500

    Add site generation in main.py

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 import os, shutil
-
+from markdown_blocks import markdown_to_html_node
+from inline_markdown import extract_title
 
 PATH_STATIC = "./static"
 PATH_PUBLIC = "./public"
+PATH_TEMPLATE = "template.html"
 
 
 def main():
     copy_files(PATH_STATIC, PATH_PUBLIC)
+    generate_page("./content/index.md", PATH_TEMPLATE, "./public/index.html")
 
 
 def copy_files(source, destination):
@@ -27,6 +30,30 @@ def copy_recursive(source, destination):
             shutil.copy(from_path, dest_path)
         else:
             copy_recursive(from_path, dest_path)
+
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    markdown = open_file(from_path)
+    template = open_file(template_path)
+    markdown_str = markdown_to_html_node(markdown).to_html()
+    title = extract_title(markdown)
+    page_html = template.replace("{{ Title }}", title).replace(
+        "{{ Content }}", markdown_str
+    )
+    with open(dest_path, "a") as html:
+        html.write(page_html)
+    html.close()
+
+
+def open_file(path):
+    try:
+        with open(path) as file:
+            return file.read()
+    except FileNotFoundError as e:
+        print(e)
+    finally:
+        file.close()
 
 
 if __name__ == "__main__":
