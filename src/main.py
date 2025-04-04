c70dea15aa8231e0c23fde40e6a9be637e316b91
Selene Moonsilver <bobdablob42+gamedev@gmail.com>
2025-04-03 20:09:00 -0400

    Update to add new stuff

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-import os, shutil
+import os, shutil, sys
 from pathlib import Path
 from markdown_blocks import markdown_to_html_node
 from inline_markdown import extract_title
@@ -9,8 +9,9 @@ PATH_TEMPLATE = "template.html"
 
 
 def main():
+    basepath = "/" if len(sys.argv) != 2 else sys.argv[1]
     copy_files(PATH_STATIC, PATH_PUBLIC)
-    generate_pages_recursive("./content/", PATH_TEMPLATE, "./public/")
+    generate_pages_recursive("./content/", PATH_TEMPLATE, "./docs/", basepath)
 
 
 def copy_files(source, destination):
@@ -33,21 +34,24 @@ def copy_recursive(source, destination):
             copy_recursive(from_path, dest_path)
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     markdown = open_file(from_path)
     template = open_file(template_path)
     markdown_str = markdown_to_html_node(markdown).to_html()
     title = extract_title(markdown)
-    page_html = template.replace("{{ Title }}", title).replace(
-        "{{ Content }}", markdown_str
+    page_html = (
+        template.replace("{{ Title }}", title)
+        .replace("{{ Content }}", markdown_str)
+        .replace('href="/', f'href="{basepath}')
+        .replace('src="/', f'src="{basepath}')
     )
     with open(dest_path, "a") as html:
         html.write(page_html)
     html.close()
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     if not os.path.exists(dest_dir_path):
         os.mkdir(dest_dir_path)
 
@@ -55,10 +59,10 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
         from_path = os.path.join(dir_path_content, file)
         dest_path = Path(os.path.join(dest_dir_path, file)).with_suffix(".html")
         if os.path.isfile(from_path):
-            generate_page(from_path, template_path, dest_path)
+            generate_page(from_path, template_path, dest_path, basepath)
             print("Done!")
         else:
-            generate_pages_recursive(from_path, template_path, dest_path)
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
 
 
 def open_file(path):
